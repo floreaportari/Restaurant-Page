9d4cc99c702f05b86c36fe8a0bf40df3cd29542c
Florian Portari <floryanportar@gmail.com>
2022-09-05 12:45:15 +0200

    updated file

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,21 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
-
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
